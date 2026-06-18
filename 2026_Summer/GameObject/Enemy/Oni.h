cff8651d6c9024e5630466b6d6592743787f525e
tw4706 <towajobseach@gmail.com>
2026-06-18 18:59:55 +0900

    鬼クラス実装

---
+++ 2026_Summer/GameObject/Enemy/Oni.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "EnemyBase.h"
+class Oni :public EnemyBase
+{
+public:
+	Oni();
+	virtual ~Oni();
+
+	void Init()override;
+	void Update()override;
+	void Draw()override;
+
+private:
+};
+
