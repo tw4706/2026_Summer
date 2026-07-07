f328a8574f89d81a2782455d836e85f2e4ea0036
tw4706 <towajobseach@gmail.com>
2026-07-07 18:59:31 +0900

    ガード時のアニメーションのバグを修正

--- 2026_Summer/GameObject/Player/PlayerStateGuard.cpp
+++ 2026_Summer/GameObject/Player/PlayerStateGuard.cpp
@@ -11,6 +11,9 @@ namespace
 	//左右のガードアニメーション
 	const std::wstring_view kPlayerLeftGuard = L"Player|LeftGuard";
 	const std::wstring_view kPlayerRightGuard = L"Player|RightGuard";
+
+	//移動速度
+	constexpr float kMoveSpeed = 5.0f;
 }
 
 PlayerStateGuard::PlayerStateGuard(std::weak_ptr<Player> pPlayer, PlayerCamera& camera) :
@@ -37,35 +40,40 @@ void PlayerStateGuard::Update()
 	//正面ベクトルを今向いている向きで定義
 	Vector3 forwardVec = { std::sinf(player->moveAngle_),0.0f,-std::cosf(player->moveAngle_) };
 
-	//移動入力があったら
-	if (playerDir.LengthSq() > 0.0001f)
+	//移動の入力がない場合はIdleに遷移
+	if (playerDir.LengthSq() <= 0.0001f)
 	{
-		//正面のベクトルと移動の入力ベクトルの外積を計算
-		Vector3 cross = forwardVec.Cross(playerDir);
+		auto nextState = std::make_shared<PlayerStateIdle>(pPlayer_, camera_);
+		player->ChangeState(nextState);
+		return;
+	}
 
-		//Y成分で右移動か左移動かを判定
-		if (cross.y_ > 0.1f)
-		{
-			player->ChangeAnimation(AnimationState::RightGuard, kPlayerRightGuard.data());
-		}
-		else if (cross.y_ < -0.1f)
-		{
-			player->ChangeAnimation(AnimationState::LeftGuard, kPlayerLeftGuard.data());
-		}
-		else //真前や真後ろなど
-		{
-			player->ChangeAnimation(AnimationState::Guard, kPlayerGuard.data());
-		}
+	playerDir.Normalize();
+
+	//移動速度を計算
+	Vector3 moveVec = playerDir * kMoveSpeed;
+	//移動後の位置を計算
+	Vector3 nextPos = player->GetPos() + moveVec;
+	//位置の適用
+	player->SetPos(nextPos);
+
+	//正面のベクトルと移動の入力ベクトルの外積を計算
+	Vector3 cross = forwardVec.Cross(playerDir);
+
+	//Y成分で右移動か左移動かを判定してアニメーションを遷移させる
+	if (cross.y_ > 0.0f)
+	{
+		player->ChangeAnimation(AnimationState::RightGuard, kPlayerRightGuard.data());
 	}
 	else
 	{
-
-		player->ChangeAnimation(AnimationState::Guard, kPlayerGuard.data());
+		player->ChangeAnimation(AnimationState::LeftGuard, kPlayerLeftGuard.data());
 	}
 
-	//ガードのアニメーションが終了したらIdle状態に戻る
+	// アニメーションが終了したら
 	if (player->IsAnimationEnd())
 	{
+		//Idleに遷移させる
 		auto nextState = std::make_shared<PlayerStateIdle>(pPlayer_, camera_);
 		player->ChangeState(nextState);
 	}
