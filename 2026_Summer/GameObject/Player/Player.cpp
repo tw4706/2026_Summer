b5cfb2d4f8f3d7217e706dece1e01aa9a49b23e3
tw4706 <towajobseach@gmail.com>
2026-07-22 14:05:45 +0900

    一旦ロックオンUIを消した

--- 2026_Summer/GameObject/Player/Player.cpp
+++ 2026_Summer/GameObject/Player/Player.cpp
@@ -201,39 +201,32 @@ void Player::Draw()
 	}
 
 	//ロックオンのレティクルUI
-	if (IsLockOn())
-	{
-		//ターゲットのロックができているなら
-		if (auto target = pLockOnEnemy_.lock())
-		{
-			//始点(始点はプレイヤーの位置から高さをプラスしている)
-			Vector3 startPos = pos_;
-			startPos.y_ += kRayStartHeight;
-
-			//終点(敵の半分くらいの位置)
-			Vector3 endPos = target->GetCameraTarget();
-
-			//DxLibのVECTOR型に変換
-			VECTOR start = startPos.ToDxlibVector();
-			VECTOR end = endPos.ToDxlibVector();
-
-			//ロックオン中は「黄色」の太い線でレイを描画
-			unsigned int rayColor = GetColor(255, 255, 0);
-			DrawLine3D(start, end, rayColor);
-
-			VECTOR reticleScreenPos = ConvWorldPosToScreenPos(end);
-
-			SetUseZBuffer3D(false);
-
-			//レティクルUIの描画
-			DrawRotaGraph3(static_cast<int>(reticleScreenPos.x - kReticleOffsetX), static_cast<int>(reticleScreenPos.y - kReticleOffsetY),
-				0, 0,
-				kReticleScale, kReticleScale,
-				0.0f, reticleUIHandle_, true);
-
-			SetUseZBuffer3D(true);
-		}
-	}
+	//if (IsLockOn())
+	//{
+	//	//ターゲットのロックができているなら
+	//	if (auto target = pLockOnEnemy_.lock())
+	//	{
+	//		//始点(始点はプレイヤーの位置から高さをプラスしている)
+	//		Vector3 startPos = pos_;
+	//		startPos.y_ += kRayStartHeight;
+	//		//終点(敵の半分くらいの位置)
+	//		Vector3 endPos = target->GetCameraTarget();
+	//		//DxLibのVECTOR型に変換
+	//		VECTOR start = startPos.ToDxlibVector();
+	//		VECTOR end = endPos.ToDxlibVector();
+	//		//ロックオン中は「黄色」の太い線でレイを描画
+	//		unsigned int rayColor = GetColor(255, 255, 0);
+	//		DrawLine3D(start, end, rayColor);
+	//		VECTOR reticleScreenPos = ConvWorldPosToScreenPos(end);
+	//		SetUseZBuffer3D(false);
+	//		//レティクルUIの描画
+	//		DrawRotaGraph3(static_cast<int>(reticleScreenPos.x - kReticleOffsetX), static_cast<int>(reticleScreenPos.y - kReticleOffsetY),
+	//			0, 0,
+	//			kReticleScale, kReticleScale,
+	//			0.0f, reticleUIHandle_, true);
+	//		SetUseZBuffer3D(true);
+	//	}
+	//}
 
 #ifdef _DEBUG
 	if (!colliders_.empty())
