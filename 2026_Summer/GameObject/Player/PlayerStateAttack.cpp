e6ed570b0f3366678f3e87f7fa851156067c6a35
tw4706 <towajobseach@gmail.com>
2026-07-24 13:31:44 +0900

    刀のエフェクトを調整

--- 2026_Summer/GameObject/Player/PlayerStateAttack.cpp
+++ 2026_Summer/GameObject/Player/PlayerStateAttack.cpp
@@ -8,7 +8,7 @@ namespace
 	const std::wstring_view kPlayerAttack = L"Player|Attack";
 
 	//刀エフェクトを再生し始めるフレーム
-	constexpr float kEffectStartFrame = 17.0f;
+	constexpr float kEffectStartFrame = 18.0f;
 
 	//刀エフェクトを止めるフレーム
 	constexpr float kEffectEndFrame = 25.0f;
