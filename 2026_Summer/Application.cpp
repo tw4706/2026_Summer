776d69492df9bf95c0b0a8e3c255274dc2ee2081
tw4706 <towajobseach@gmail.com>
2026-09-04 09:57:23 +0900

    動作確認

--- 2026_Summer/Application.cpp
+++ 2026_Summer/Application.cpp
@@ -27,7 +27,7 @@ Application& Application::GetInstance()
 bool Application::Init()
 {
 	//ウィンドウモード設定
-	ChangeWindowMode(true);
+	ChangeWindowMode(false);
 
 	//ウィンドウのタイトル変更
 	SetMainWindowText(L"SamuraiAdapt");
